d99fe1b3307e9b6f17bfde37df6433686b1b0a24
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2023-01-13 13:39:32 -0800

    working on making call to openai server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let express = require("express");
-let bp = require("body-parser");
+const http = require("http");
+const express = require("express");
+const bp = require("body-parser");
+const generate = require('./api/generate.js')
 
 const app = express();
 app.use(bp.urlencoded({ extended: true }));
@@ -8,7 +10,8 @@ app.use(bp.json());
 app.use(express.static("./public"));
 
 app.post("/createPlan", async (req, res) => {
-  console.log("post request", req.body);
+    console.log("post request", req.body);
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) =>
 });
 
 app.listen(3000);
