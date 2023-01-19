6718e34f5b58e90e1a6158a781e7ca08ca5baab1
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2023-01-19 08:44:02 -0800

    added more user inputs

--- api/generate.js
+++ api/generate.js
@@ -10,9 +10,11 @@ const openai = new OpenAIApi(configuration);
 const openaiCall = async function (req, res) {
   const inputs = req.body || "";
   console.log("post openaiCall", inputs);
+  let [userTopic, userGrade, userClassLength, userState] = inputs;
+  // console.log(userTopic, userClassLength, userState);
 
-  function generatePrompt(inputs) {
-    return `Please create a 1 hour lesson plan for 5th graders about ${inputs} which abides by california curriculum standards. Insert examples of code, diagrams, or pictures when possible. Use Madeline Hunter's template.`;
+  function generatePrompt(userTopic, userGrade, userClassLength, userState) {
+    return `Please create a ${userClassLength} lesson plan for ${userGrade} about ${userTopic} which abides by ${userState} curriculum standards. Insert examples of code, diagrams, or pictures when possible. Use Madeline Hunter's template.`;
   }
 
   const completion = await openai.createCompletion({
