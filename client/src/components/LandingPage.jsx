6568e6160da7ee3cf5e12d42829db1d22381b963
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2023-04-11 10:12:11 -0700

    created jsx form for landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import {useState} from "react";
 
 function LandingPage() {
+    const [signinUsername, setsigninUsername] = useState("")
+    const [signinPassword, setsigninPassword] = useState("")
+
     return (
-        <h1>This is the homepage!</h1>
+        <section>
+            <h1>This is the homepage!</h1>
+            <div id='signinForm' className="d-flex justify-content-center col-12">
+            <form className="d-flex flex-column justify-content-center align-content-around" action="/login" method="get"
+                id="signinForm" onSubmit={e => {
+                e.preventDefault();
+            }}
+            >
+                <div className="my-3">
+                    <label>Username:</label>
+                    <input id="signinUsername" className="form-control" placeholder='Type you work email here...' type="text" name="signinUsername" value={signinUsername} onChange={e => setsigninUsername(e.target.value)}></input>
+                    </div>
+                <div className="my-3">
+                    <label>Password:</label>
+                    <input id="signinPassword" className="form-control" placeholder='Type you work password here...' type="text" name="signinPassword" value={signinPassword} onChange={e => setsigninPassword(e.target.value)}></input>
+                </div>
+               
+                <button className="mb-3 btn btn-primary" type="submit">Sign In</button>
+            </form>
+        </div>
+        </section>
+        
     )
 }
 
